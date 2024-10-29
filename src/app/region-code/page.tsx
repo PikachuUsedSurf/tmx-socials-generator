2bdac90cef6e52061388cd6e14eac4931de98cb8
PikachuUsedSurf <nanaosafokissi@gmail.com>
2024-10-29 16:33:33 +0300

    reguon code tests

--- src/app/region-code/page.tsx
+++ src/app/region-code/page.tsx
@@ -1,16 +1,22 @@
-const RegionName = ["mtwara"];
+type Region = {
+  name: string;
+  code: string;
+};
 
-const RegionCode = ["MTWR"];
+const Regions: Region[] = [{ name: "MTWARA", code: "MTWR" }];
 
 export default function RegionC() {
   return (
     <>
       <div>
-        <div className=" border rounded-md text-center p-4 w-40 h-40">
-          {RegionName.map((region, index) => (
-            <div key={index}>{region}</div>
-          ))}
-        </div>
+        {Regions.map((region, index) => (
+          <div className=" flex text-center justify-center items-center  border rounded-md shadow-lg p-4 w-40 h-40 space-y-4 ">
+            <div key={index}>
+              <h1>{region.name}</h1>
+              <h2>{region.code}</h2>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
