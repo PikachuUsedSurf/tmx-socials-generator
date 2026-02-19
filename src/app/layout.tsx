3718864949a0cf32de867173386b97e7c4e38f6d
PikachuUsedSurf <nanaosafokissi@gmail.com>
2026-02-19 12:24:56 +0300

    hydration fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <SidebarProvider>
           <div className="flex w-screen h-screen">
             <AppSidebar />
